8f0af9eafca5dca2c2a5eed7d22a4cdb5cb9c8d2
蹇胜-绛门 <pfjhetg@139.com>
2017-09-18 14:01:03 +0800

    first commit

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,15 +1,45 @@
-//
-//  main.cpp
-//  LearnOpenGL
-//
-//  Created by pfjhetg on 2017/9/18.
-//  Copyright © 2017年 pfjhetg. All rights reserved.
-//
-
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void Render(void)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    {
+        glColor3f(1.0,0.0,0.0);
+        glVertex2f(0, .5);
+        glColor3f(0.0,1.0,0.0);
+        glVertex2f(-.5,-.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(.5, -.5);
+    }
+    glEnd();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    GLFWwindow* win;
+    if(!glfwInit()){
+        return -1;
+    }
+    win = glfwCreateWindow(640, 480, "OpenGL Base Project", NULL, NULL);
+    if(!win)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if(!glewInit())
+    {
+        return -1;
+    }
+    glfwMakeContextCurrent(win);
+    while(!glfwWindowShouldClose(win)){
+        Render();
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
     return 0;
 }
