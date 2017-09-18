4bebd8ec9e605701cde0c8f0e6310f3d30bf8886
蹇胜-绛门 <pfjhetg@139.com>
2017-09-18 11:38:49 +0800

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by pfjhetg on 2017/9/18.
+//  Copyright © 2017年 pfjhetg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
